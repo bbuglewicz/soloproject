9c25147c4c455ea52b8067c0c45b52dca635dd75
Brendan Buglewicz <bcbuglewicz@unomaha.edu>
2020-10-07 15:50:34 -0500

    Hello World printed to webpage

--- contactlistdb/src/Servlet.java
+++ contactlistdb/src/Servlet.java
@@ -14,9 +14,10 @@ public class Servlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().println("Hello World");
 	}
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
